b70dd8ed628954bf76289d97319a70fbac9c61e0
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-04-03 00:51:05 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates(:content, { :length => { :minimum => 250 } })
+    validates(:summary, { :length => { :maximum => 250 } })
+    #validates_inclusion_of :format, in: %w( jpg gif png ), message: "extension %{value} is not included in the list"
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :clickbait_enough?
+
+   PHRASES = [
+    /Won't Believe/i,
+    /Top [0-9]*/i,
+    /Secret/i,
+    /Guess/
+  ]
+
+    def clickbait_enough?
+    if PHRASES.none? {|cb| cb.match title}
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
 end
